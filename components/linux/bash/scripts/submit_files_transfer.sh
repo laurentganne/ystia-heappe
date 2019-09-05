6097b2a1b33702922f427260dfcce329412a0062
Laurent Ganne <ganne.laurent@gmail.com>
2019-09-05 14:41:24 +0000

    Updated components

---
+++ components/linux/bash/scripts/submit_files_transfer.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "TODO: Get files updated by job $JOB_ID..."
+
+export TOSCA_JOB_ID=0
--- components/linux/bash/scripts/wait_end_of_transfer.sh
+++ components/linux/bash/scripts/wait_end_of_transfer.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo "TODO: wait for the end of files transfer..."
+
 export TOSCA_JOB_STATUS="COMPLETED"
 
 echo "Job status: $TOSCA_JOB_STATUS"
